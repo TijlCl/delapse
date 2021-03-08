389f51b526f1b67854678316110a4f6a47510323
tijlclottemans <tijlclottemans@gmail.com>
2021-03-08 12:27:41 +0100

    Setup for the websockets

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TestEvent;
+use App\Jobs\TestJob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
 
     public function test()
     {
-        $test =null;
     }
 }
