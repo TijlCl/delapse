04c3574a9555bbef74a514978ecc32ece221e992
seppeclottemans <seppe.clottemans@student.ehb.be>
2021-03-13 16:22:14 +0100

    feat(events): add event setup

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+
+
+    public function store()
+    {
+        $test = 0;
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+}
