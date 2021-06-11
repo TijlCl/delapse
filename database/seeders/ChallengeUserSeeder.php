348282252a70e8d6c406eabc26508ff19abeb2e4
seppeclottemans <seppe.clottemans@student.ehb.be>
2021-06-11 13:34:22 +0200

    build(challenge-user-seeder): add uncompleted challenges

--- database/seeders/ChallengeUserSeeder.php
+++ database/seeders/ChallengeUserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Http\Repositories\ChallengeRepository;
+use App\Http\Repositories\UserRepository;
 use App\Models\Challenge;
 use App\Models\User;
 use Carbon\Carbon;
@@ -9,6 +11,16 @@ use Illuminate\Database\Seeder;
 
 class ChallengeUserSeeder extends Seeder
 {
+
+    private $challengeRepository;
+    private $userRepository;
+
+    public function __construct(ChallengeRepository $challengeRepository, UserRepository $userRepository)
+    {
+        $this->challengeRepository = $challengeRepository;
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * Run the database seeds.
      *
@@ -16,11 +28,19 @@ class ChallengeUserSeeder extends Seeder
      */
     public function run()
     {
+        //give user completed challenges
         $user = User::where('id', 1)->first();
 
         $challenges = Challenge::all();
         $challenges = $challenges->pluck('id')->toArray();
 
         $user->challenges()->syncWithPivotValues($challenges, ['completed_at' => Carbon::now()]);
+
+        // give user uncompleted challenges
+        $weeklyChallenges = Challenge::inRandomOrder()
+            ->limit(3)
+            ->get();
+
+        $this->userRepository->addWeeklyChallenges($user, $weeklyChallenges);
     }
 }
